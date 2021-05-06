37d3682d64c9b883cc652d015634e2e8bd50804f
Keita Urashima <ursm@ursm.jp>
2021-05-07 00:37:19 +0900

    fix ASDF_DATA_DIR

--- config/zsh/rc.d/asdf.zsh
+++ config/zsh/rc.d/asdf.zsh
@@ -1,8 +1,9 @@
+export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
+
 znap source asdf-vm/asdf asdf.sh
 
 fpath+=(~[asdf]/completions)
 
-export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
 export ASDF_GEM_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/asdf/default-gems
 export ASDF_NPM_DEFAULT_PACKAGES_FILE=$XDG_CONFIG_HOME/asdf/default-npm-packages
 export RUST_WITHOUT=rust-docs
