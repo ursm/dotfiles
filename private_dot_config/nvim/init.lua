ef629dedfd3912db07ec9d377f626d72c9c22075
Keita Urashima <ursm@ursm.jp>
2025-10-26 13:33:26 +0900

    Update

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -49,7 +49,8 @@ vim.api.nvim_create_autocmd('QuickFixCmdPost', {
 vim.filetype.add({
   extension = {
     axlsx = 'ruby',
-    jb    = 'ruby'
+    jb    = 'ruby',
+    jsonl = 'json'
   },
 
   filename = {
